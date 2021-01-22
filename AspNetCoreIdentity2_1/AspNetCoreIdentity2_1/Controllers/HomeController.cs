d4b17bd47544416a46106191cc367712da3aeea3
Thiago Torres <thiago.torres.ter@estacio.br>
2021-01-22 19:34:30 -0300

    Subida Projeto

---
+++ AspNetCoreIdentity2_1/AspNetCoreIdentity2_1/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using AspNetCoreIdentity2_1.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreIdentity2_1.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Secreto()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
